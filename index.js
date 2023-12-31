8c7d74df9601dd28e9947223e08438b939d91dc7
sidezbros <ts.sides@gmail.com>
2023-12-31 16:45:18 -0600

    add sql + cleanup

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
-const routes = require('./routes/api');
 const path = require("path");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+const router = express.Router();
+
+router.get('/bingo_card/:name', (req, res, next) => {
+    if(req.params.name){
+        //Search DB where name
+        res.json({result: 'Request for '+req.params.name+' received.'})
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+router.post('/update_card/:name', (req, res, next) => {
+    if (req.params.name && req.body) {
+        res.json({result: 'working!'})
+    } else {
+        res.sendStatus(400);
+    }
+});
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -17,7 +34,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, './client/build')));
 
-app.use('/api', routes);
+app.use('/api', router);
 
 app.use((err, req, res, next) => {
     console.log(err);
