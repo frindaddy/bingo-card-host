518453cf1d50f387b7014cc20eb2dd1c36b2cf44
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-02-19 13:56:42 -0600

    cleanup validation and routes

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000;
 const JSON_DIR = process.env.JSON_DIR || './client/src/cards/';
 
 const DISCORD_WEBHOOK = process.env.DISCORD_WEBHOOK;
-let warnNoDiscordWebhook = true;
 
 let validDBs = []
 let players = {}
@@ -20,6 +19,7 @@ const db2025 = new JsonDB(new Config(JSON_DIR+"2025", true, true, '/'));
 
 validateDB('2024');
 validateDB('2025');
+validateBotWebhook();
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -57,6 +57,14 @@ function validateDB(year){
     })
 }
 
+function validateBotWebhook() {
+    if(DISCORD_WEBHOOK !== undefined){
+        console.log('Webhook detected. Server will attempt to send Discord bot messages.');
+    } else {
+        console.log('No webhook detected. No Discord bot messages will be attempted.');
+    }
+}
+
 router.get('/bingo_card/players/:year',  (req, res, next) => {
     res.json({players: players[req.params.year]});
 });
@@ -80,21 +88,30 @@ router.get('/bingo_card/:year_name',  (req, res, next) => {
 });
 
 router.post('/update_card/:year', (req, res, next) => {
-    if (req.params.year && req.body && validDBs.includes(req.params.year)) {
-        if(players[req.params.year].map(player =>  {return player[0]}).includes(req.body.name)){
+    if(validUpdateRequest(req)){
+        //Update card function if discord is enabled. Timing on calls is important.
+        if(DISCORD_WEBHOOK !== undefined) {
             getJsonDB(req.params.year).getData('/' + req.body.name + '/selectedTiles').then((oldSelectedTiles) => {
-                if(oldSelectedTiles < (newSelectedTiles = req.body.selectedTiles)){
+                let newSelectedTiles = req.body.selectedTiles
+                if(oldSelectedTiles !== newSelectedTiles){
                     messageBot(req.params.year, req.body.name, oldSelectedTiles, newSelectedTiles);
                 }
+                getJsonDB(req.params.year).push('/' + req.body.name + '/selectedTiles', newSelectedTiles).then(r => res.sendStatus(200));
             });
-            getJsonDB(req.params.year).push('/' + req.body.name + '/selectedTiles', req.body.selectedTiles).then(r => res.sendStatus(200));
         } else {
-            res.sendStatus(400);
+            //Regular update call if there is no discord bot
+            getJsonDB(req.params.year).push('/' + req.body.name + '/selectedTiles', req.body.selectedTiles).then(r => res.sendStatus(200));
         }
     } else {
         res.sendStatus(400);
     }
 });
+
+function validUpdateRequest(req) {
+    let isYearValid = req.params.year && req.body && validDBs.includes(req.params.year);
+    let doesPlayerExist = players[req.params.year].map(player =>  {return player[0]}).includes(req.body.name);
+    return isYearValid && doesPlayerExist;
+}
 
 function messageBot(year, name, oldSelectedTiles, newSelectedTiles){
     getJsonDB(year).getData('/'+ name +'/displayName').then((displayName) => {
@@ -126,19 +143,14 @@ function sendDiscordMessage(markedTileText, displayName){
             })
                 .then((response) => {
                     if (!response.ok) {
-                        console.log("Error sending Discord message: "+response);
+                        console.error("Error sending Discord message: "+response);
                     }
                 })
                 .catch((error) => {
                     console.error(error);
                 });
         } catch (e) {
-            console.error('Fetch failed. Check that API key is valid and discord is up!');
-        }
-    } else {
-        if(tryDiscordPost){
-            console.log("Discord message not posted because there is no provided Discord webhook.");
-            warnNoDiscordWebhook = false;
+            console.error('Webhook failed. Check that API key is valid and Discord is up!');
         }
     }
 }
